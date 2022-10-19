48acac6a9367d0cc546c3353d4567e7ffe43b9c8
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2022-10-19 19:33:47 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-// connect mongoDB
+// Extra Middleware security packages
+const helmet = require('helmet');
+// CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+const cors = require('cors');
+// Sanitize untrusted HTML (to prevent XSS) with a configuration specified by a Whitelist
+const xss = require('xss');
+// Basic rate-limiting middleware for Express. Use to limit repeated requests to public APIs and/or endpoints such as password reset
+const rateLimiter = require('express-rate-limit');
+
+// Middleware
+const authenticateUser = require('./middleware/authentication');
+
+// Connect DB
 const connectDB = require('./db/connect');
 
-// routers (routes + controllers)
+// Routers
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
@@ -16,12 +28,23 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
+// Heroku is the reverse proxy for us
+app.set('trust proxy', 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	})
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+// authenticateUser = middleware we use for authenticating users before we let proceed to the jobs routes
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
-// setting node to use this middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -31,7 +54,7 @@ const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URI);
 		app.listen(port, () =>
-			console.log(`Server is listening on port ${port}...`)
+			console.log(`Server is listening on port ${port} 😎🤟...`)
 		);
 	} catch (error) {
 		console.log(error);
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,30 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+	let customError = {
+		// Set default (for the errors that I didn't customize  a head)
+		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+		msg: err.message || 'Something went wrong, please try again later',
+	};
 
-module.exports = errorHandlerMiddleware
+	if (err.name === 'ValidationError') {
+		customError.msg = Object.values(err.errors)
+			.map(item => item.message)
+			.join(',');
+		customError.statusCode = StatusCodes.BAD_REQUEST;
+	}
+	if (err.code && err.code === 11000) {
+		customError.msg = `Duplicate value entered for ${Object.keys(
+			err.keyValue
+		)} field 🤨. Please choose another ${Object.keys(err.keyValue)}.`;
+		customError.statusCode = StatusCodes.BAD_REQUEST;
+	}
+
+	if (err.name === 'CastError') {
+		customError.msg = `No item with ID ${err.value} was found 😩`;
+		customError.statusCode = StatusCodes.NOT_FOUND;
+	}
+	// return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+	return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
